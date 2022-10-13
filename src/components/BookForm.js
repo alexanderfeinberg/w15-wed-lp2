bf8eed61c48c37f0d4e34c17246e503ba306fcd7
alexander1fein <feinberg.alexander@gmail.com>
2022-10-12 20:46:10 -0400

    Phase5

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,39 +1,40 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addBook } from "../store/booksReducer";
 
 const BookForm = ({ book, formType }) => {
   const history = useHistory();
+  let dispatch = useDispatch();
   const [title, setTitle] = useState(book.title);
   const [author, setAuthor] = useState(book.author);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     book = { ...book, title, author };
+    dispatch(addBook(book));
 
     history.push(`/books/${book.id}`);
   };
 
   return (
-    <form onSubmit={handleSubmit} >
+    <form onSubmit={handleSubmit}>
       <h2>{formType}</h2>
       <label>
         Title
-        <input 
+        <input
           type="text"
           value={title}
-          onChange={e => setTitle(e.target.value)}
+          onChange={(e) => setTitle(e.target.value)}
         />
       </label>
       <label>
         Author
-        <textarea 
-          value={author}
-          onChange={e => setAuthor(e.target.value)}
-        />
+        <textarea value={author} onChange={(e) => setAuthor(e.target.value)} />
       </label>
       <input type="submit" value={formType} />
     </form>
   );
-}
+};
 
 export default BookForm;
--- src/store/booksReducer.js
+++ src/store/booksReducer.js
@@ -3,6 +3,7 @@ import books from "../data/initial-books.json";
 const LOAD_BOOKS = "books/loadBooks";
 const LOAD_BOOK = "books/loadBook";
 const DELETE_BOOK = "books/deleteBook";
+const ADD_BOOK = "books/addBook";
 
 export const loadBooks = () => {
   return { type: LOAD_BOOKS };
@@ -14,6 +15,10 @@ export const deleteBookAction = (book) => {
 
 export const loadBook = (book) => {
   return { type: LOAD_BOOK, book: book };
+};
+
+export const addBook = (book) => {
+  return { type: ADD_BOOK, book: book };
 };
 
 const normalizeData = (data = books) => {
@@ -34,6 +39,8 @@ const booksReducer = (state = initialState, action) => {
       return newState;
     case LOAD_BOOK:
       return { ...state };
+    case ADD_BOOK:
+      return { ...state, [action.book.id]: action.book };
     default:
       return state;
   }
